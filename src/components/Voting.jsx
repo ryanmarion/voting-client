268f145b14dbd7f8de30820a609350e48b252e24
Ryan M <ryanmarion012@gmail.com>
2019-04-04 17:08:08 -0600

    major updates to packages to get webpack working. works effectively and react is rendering to the DOM.

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
-export default React.createClass({
+var createReactClass = require('create-react-class');
+
+module.exports=createReactClass({
   getPair: function() {
-    return this.props.pair || [];
-  },
-  render: function() {
-    return <div className="voting">
-      {this.getPair().map(entry =>
-        <button key={entry}>
-          <h1>{entry}</h1>
-        </button>
-      )}
-    </div>;
-  }
+      return this.props.pair || [];
+    },
+    render: function() {
+      return <div className="voting">
+        {this.getPair().map(entry =>
+          <button key={entry}>
+            <h1>{entry}</h1>
+          </button>
+        )}
+      </div>;
+    }
 });
