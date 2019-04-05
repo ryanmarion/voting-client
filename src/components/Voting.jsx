64d9e762e67fe40d73baff1fc8ae5e6817bbe453
Ryan M <ryanmarion012@gmail.com>
2019-04-04 20:35:01 -0600

    testing functionality is there

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 
-var createReactClass = require('create-react-class');
-
-module.exports=createReactClass({
+export default React.createClass({
   getPair: function() {
-      return this.props.pair || [];
-    },
-    render: function() {
-      return <div className="voting">
-        {this.getPair().map(entry =>
-          <button key={entry}>
-            <h1>{entry}</h1>
-          </button>
-        )}
-      </div>;
-    }
+    return this.props.pair || [];
+  },
+  render: function() {
+    return <div className="voting">
+      {this.getPair().map(entry =>
+        <button key={entry}>
+          <h1>{entry}</h1>
+        </button>
+      )}
+    </div>;
+  }
 });
